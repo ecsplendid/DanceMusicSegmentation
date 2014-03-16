c4bfda7bcec6328d5bfbfc9f5c1f2883bd9657c9
Tim Scarfe <tim+github@developer-x.com>
2014-03-16 20:30:34 +0000

    First set of results, and adding a no gauss set of results

---
+++ Matlab/experiments/A_nogauss.m
@@ -0,0 +1,4 @@
+use_costsymmetry = 1;
+use_costcontig = 1;
+use_costsum = 1;
+use_costgaussian = 0;
--- Matlab/experiments/run_resultsforpaper.m
+++ Matlab/experiments/run_resultsforpaper.m
@@ -7,44 +7,47 @@ cd ..
 config_settings;
 cd experiments
 
-tile_sizes = [30 20 10 5];
+tile_sizes = [30 20 10];
 
 for i=1:length(tile_sizes)
 
     secondsPerTile = tile_sizes(i);
 
-    A;
+    A_nogauss;
     cd ..
     run_experiments;
-    name = sprintf('A_T%d',secondsPerTile);
+    name = sprintf('A_nogauss_T%d',secondsPerTile);
     cd experiments
     save_result;
     
-    B;
+    B_nogauss;
     cd ..
     run_experiments;
-    name = sprintf('B_T%d',secondsPerTile);
+    name = sprintf('B_nogauss_T%d',secondsPerTile);
     cd experiments
     save_result;
 
-    C;
+    C_nogauss;
     cd ..
     run_experiments;
-    name = sprintf('C_T%d',secondsPerTile);
+    name = sprintf('C_nogauss_T%d',secondsPerTile);
     cd experiments
     save_result;
     
-    D;
+    D_nogauss;
     cd ..
     run_experiments;
-    name = sprintf('D_T%d',secondsPerTile);
+    name = sprintf('D_nogauss_T%d',secondsPerTile);
     cd experiments
     save_result;
 
-    E;
-    cd ..
-    run_experiments;
-    name = sprintf('E_T%d',secondsPerTile);
-    cd experiments
-    save_result;
 end
+
+secondsPerTile = 5;
+
+A;
+cd ..
+run_experiments;
+name = sprintf('A__T%d',secondsPerTile);
+cd experiments
+save_result;
