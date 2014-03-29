b5644dd02f02234b2a3845eb8ca0acc08cb9d27a
Tim Scarfe <tim+github@developer-x.com>
2014-03-29 23:00:01 +0000

    updated paper, new results, new experiments

--- Matlab/config_settings.m
+++ Matlab/config_settings.m
@@ -2,11 +2,11 @@
 
 % Feature Extraction Parameters
 sampleRate = 4000;
-secondsPerTile = 15;
+secondsPerTile = 5;
 minTrackLength = 180;
 maxExpectedTrackWidth = 370*2;   % (magicisland=380*2 others 350*2)
 bandwidth = 5; % bandwith for the width of the convolution filter
-lowPassFilter = 1500; %Hz
+lowPassFilter = 1662; %Hz
 highPassFilter = 80; %Hz
 gaussian_filterdegree = 2; % for the convolution filter on FFT result
 
@@ -34,17 +34,17 @@ use_costgaussianwidth = 1;
 % the symmetry and contig cost matrices. >1 shifts mean higher
 cosine_normalization = 1;
 % contig_windowsize in seconds
-contig_windowsize = 30;
+contig_windowsize = 8;
 
 % which cost functions to use and how much weight do they have (>0)
 % what incentive balance do the respective cost functions have [0,1]
-use_costsymmetrysum     = 1;        costsymmetrysum_incentivebalance = 0.5;
-use_costsymmetrydiff    = 0;       costsymmetrydiff_incentivebalance = 0.5;
-use_costsymmetry        = 0;           costsymmetry_incentivebalance = 0.5;
-use_costcontigpast      = 0;         costcontigpast_incentivebalance = 0.5;
-use_costcontigfuture    = 0;       costcontigfuture_incentivebalance = 0.5;
-use_costsum             = 0;                costsum_incentivebalance = 0.5;
-use_costgaussian        = 0;              costgauss_incentivebalance = 0.5;
-use_costcontigevolution = 0;          costevolution_incentivebalance = 0.5;
+use_costsymmetrysum     = 0.3;        costsymmetrysum_incentivebalance = 0.7;
+use_costsymmetrydiff    = 0.9;       costsymmetrydiff_incentivebalance = 0.8;
+use_costsymmetry        = 0.1;           costsymmetry_incentivebalance = 0.3;
+use_costcontigpast      = 0.9;         costcontigpast_incentivebalance = 0.1;
+use_costcontigfuture    = 0.9;       costcontigfuture_incentivebalance = 0.5;
+use_costsum             = 0.9;                costsum_incentivebalance = 0.5;
+use_costgaussian        = 0.5;              costgauss_incentivebalance = 0.8;
+use_costcontigevolution = 0.2;          costevolution_incentivebalance = 0.3;
 
 
---
+++ Matlab/experiments/run_fixedresults.m
@@ -0,0 +1,51 @@
+clear all;
+
+cd ../dataset 
+local_testset
+cd ..
+
+config_settings;
+cd experiments
+
+
+
+    secondsPerTile = 20;
+    
+    solution_shift = floor((rand*3)-1);
+    
+    cosine_normalization = rand+0.5;
+    contig_windowsize = floor(rand*80)+5;
+    
+use_costsymmetrysum     = rand;        costsymmetrysum_incentivebalance = rand;
+use_costsymmetrydiff    = rand;       costsymmetrydiff_incentivebalance = rand;
+use_costsymmetry        = rand;           costsymmetry_incentivebalance = rand;
+use_costcontigpast      = rand;         costcontigpast_incentivebalance = rand;
+use_costcontigfuture    = rand;       costcontigfuture_incentivebalance = rand;
+use_costsum             = rand;                costsum_incentivebalance = rand;
+use_costgaussian        = rand;              costgauss_incentivebalance = rand;
+use_costcontigevolution = rand;          costevolution_incentivebalance = rand;
+   
+    bandwidth = floor(rand*10)+1; % bandwith for the width of the convolution filter
+    lowPassFilter = floor(rand*1100)+800; %Hz
+    highPassFilter = floor((rand * 50)+50); %Hz
+   
+    cd ..
+    run_experiments;
+    
+    name = sprintf('T:%.2fs CN:%.1f BW:%d LPF:%d HPF:%d SFT:%d SS:%.1f@i%.1f SD:%.1f@i%.1f SY:%.1f@i%.1f CP:%.1f@i%.1f CF:%.1f@i%.1f SU:%.1f@i%.1f GA:%.1f@i%.1f CE:%.1f@i%.1f CONT:%d',...
+           secondsPerTile, cosine_normalization, bandwidth,lowPassFilter, highPassFilter, solution_shift, ...
+            use_costsymmetrysum, costsymmetrysum_incentivebalance, ...
+            use_costsymmetrydiff, costsymmetrydiff_incentivebalance, ...
+            use_costsymmetry, costsymmetry_incentivebalance, ...
+            use_costcontigpast, costcontigpast_incentivebalance, ...
+            use_costcontigfuture, costcontigfuture_incentivebalance, ...
+            use_costsum, costsum_incentivebalance, ...
+            use_costgaussian, costgauss_incentivebalance, ...
+            use_costcontigevolution, costevolution_incentivebalance, ...
+            contig_windowsize );
+    
+    
+    cd experiments
+    save_result;
+
+end
