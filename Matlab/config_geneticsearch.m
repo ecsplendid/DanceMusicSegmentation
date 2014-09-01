35a47a9075cc47dc8744801c89e7f18bcb1308de
Tim Scarfe <tim+github@developer-x.com>
2014-09-01 23:01:15 +0100

    genetic search all from code

--- Matlab/config_geneticsearch.m
+++ Matlab/config_geneticsearch.m
@@ -1,12 +1,20 @@
 
+options = gaoptimset;
 
-
+options = gaoptimset(options,'PopulationSize', 50);
+options = gaoptimset(options,'EliteCount', 10);
+options = gaoptimset(options,'Generations', 20);
+options = gaoptimset(options,'StallGenLimit', 5);
+options = gaoptimset(options,'Display', 'iter');
+options = gaoptimset(options,'PlotFcns', {  @gaplotbestf @gaplotbestindiv @gaplotdistance @gaplotexpectation @gaplotgenealogy @gaplotrange @gaplotscorediversity @gaplotscores @gaplotselection @gaplotstopping @gaplotmaxconstr });
+options = gaoptimset(options,'Vectorized', 'off');
+options = gaoptimset(options,'UseParallel', 1 );
 
 x = ga( @optimise_function, ...
     21, ... % num constraints
     [],[],[],[], ...
-    config_optimdrivebounds_lowerbounds(), ...
+    config_optimdrivebounds_lowerbounds, ...
     config_optimdrivebounds_upperbounds, ...
     [], ...
     [14,15,16,17,18,19,20,21], ... % int constraints
-    gaoptimset('UseParallel',1) );
+    options );
