6139ed34812b03446483b9399e8828ab261ab5b4
Tim Scarfe <tim+github@developer-x.com>
2014-09-22 22:04:58 +0100

    foote novelty function implemented, +more
    * shift histogram visualization
    * renamed mean based results

---
+++ Matlab/get_noveltyfunction.m
@@ -0,0 +1,97 @@
+function f = get_noveltyfunction( ...
+    show_results, w, ...
+    min_distance, threshold, draw_plot )
+% get_noveltyfunction create a novelty function along the lines suggested
+% by foote et al in his papers, run a gaussian tapered checkerboard kernel
+% down the diagonal of the cosines matrix. Then do peak finding subject to
+% constraints (threshold and minimum radius).
+% check out "AUDIO NOVELTY-BASED SEGMENTATION OF MUSIC CONCERTS"
+% (Badawy,2013) for a clear run through
+
+if nargin < 2
+   w = 120; 
+end
+
+if nargin < 3
+   min_distance = 60;
+end
+
+if nargin < 3
+   threshold = 0.3;
+end
+
+if nargin < 4
+   draw_plot = 1;
+end
+
+%%
+C = show_results.show.CosineMatrix;
+
+T = size(C,1);
+
+novelty = nan(T,1);
+
+C(isnan(C)) = 0;
+
+k = kron( [1 -1; -1 1], ones(w/2,w/2) );
+g = fspecial('gaussian', [w w], w);
+g = (g-min(min(g)))./(max(max(g))-min(min(g)));
+k = k.*g;
+
+% we need to make a bigger C, zero pad it
+% so that we can slide the kernel from start to end
+C2 = zeros( size(C)+w );
+
+C2( floor(w/2):end-floor(w/2)-1, ...
+    floor(w/2):end-floor(w/2)-1 ) = C;
+
+for t=1:T
+   
+    sq = C2( t:t+w-1, t:t+w-1 );
+    r = corrcoef(sq, k);
+    novelty( t ) = -r(1,2);
+end
+
+% normalize onto [0,1] interval
+novelty = (novelty - min(novelty)) ./ ...
+    ( max(novelty) - min(novelty) );
+
+if draw_plot
+    figure(2)
+    plot(novelty,'k', 'LineWidth', 2)
+    hold on;
+end
+
+[ p, f ] = findpeaks( novelty, ...
+    'MINPEAKDISTANCE', min_distance );
+
+f = f(f>threshold);
+
+if draw_plot
+    % plot the peaks
+    plot( f, p, 'kx', 'LineWidth', 10 );
+    % plot the threshold line
+    plot( 1:T, ones(T,1).*threshold, 'k--', 'LineWidth', 2 );
+
+    actual_tilespace = floor( ...
+        show_results.show.indexes ./ ...
+        show_results.config.secondsPerTile );
+
+    for i=1:length(actual_tilespace)
+
+        plot( [actual_tilespace(i) actual_tilespace(i)], ...
+            [ min(novelty) max(novelty) ], 'k:', 'LineWidth', 1 );
+    end
+
+    title( sprintf( ...
+'Novelty Peak Finding\n Show: %s\nMinimum Peak Distance: %d, Kernel Size: %d, Threshold: %0.1f', ...
+        show_results.show.showname, min_distance, w, threshold ) );
+
+    ylabel( 'Normalized Correlation Coefficient' );
+    xlabel('Tiles')
+
+    hold off;
+    axis tight
+end
+
+end
