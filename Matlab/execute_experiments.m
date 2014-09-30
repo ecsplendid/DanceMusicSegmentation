b084df090d809a39be72e8d51690c542574f0b68
Tim Scarfe <tim+github@developer-x.com>
2014-09-30 15:07:22 +0100

    update
    *some experiments reran over night
    *paper updates (more literature review and revised introduction/abstract)

--- Matlab/execute_experiments.m
+++ Matlab/execute_experiments.m
@@ -1,4 +1,4 @@
-function execute_experiments
+%function execute_experiments
 % run all the basic experiments from the best configs
 % for mixture, sum, evo, contig, sym.
 % no track estimation or posteior
@@ -6,31 +6,31 @@ function execute_experiments
 % sums first
 c = load('config/mean/config_sum-gauss_22-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr3 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr3 = run_experiments( c.config, 'meansum', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_mean_sum_%s', date ), 'agr3' );
 
 c = load('config/mean/config_all_mn_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr = run_experiments( c.config, 'meanall', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_mean_all_%s', date ), 'agr' );
 
 c = load('config/mean/config_contig-gauss_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr1 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr1 = run_experiments( c.config, 'meancont', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_mean_contig_%s', date ), 'agr1' );
 
 c = load('config/mean/config_evo-gauss_22-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr2 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr2 = run_experiments( c.config, 'meanev', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_mean_evo_%s', date ), 'agr2' );
 
 c = load('config/mean/config_sym-gauss_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr4 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr4 = run_experiments( c.config, 'meansy', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_mean_sym_%s', date ), 'agr4' );
 
@@ -38,33 +38,33 @@ save( sprintf( 'results/agres_mean_sym_%s', date ), 'agr4' );
 
 c = load('config/median/best-median.mat');
 c.config.compute_confs = 1;
-agr5 = run_experiments( c.heur, config_getdefaultsegcalculation, ...
+agr5 = run_experiments( c.heur, 'hesum', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_median_all_%s', date ), 'agr5' );
 
 c = load('config/median/config_heur_contig-gauss_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr6 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr6 = run_experiments( c.config, 'heal', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_median_contig_%s', date ), 'agr6' );
 
 c = load('config/median/config_heur_evo-gauss_22-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr7 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr7 = run_experiments( c.config, 'hevo', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_median_evo_%s', date ), 'agr7' );
 
 c = load('config/median/config_heur_sum-gauss_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr8 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr8 = run_experiments( c.config, 'hesum', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_median_sum_%s', date ), 'agr8' );
 
 c = load('config/median/config_heur_sym-gauss_21-Sep-2014.mat');
 c.config.compute_confs = 1;
-agr9 = run_experiments( c.config, config_getdefaultsegcalculation, ...
+agr9 = run_experiments( c.config, 'hesy', config_getdefaultsegcalculation, ...
     config_getbestnoveltyconfig, 1 )
 save( sprintf( 'results/agres_median_sym_%s', date ), 'agr9' );
 
 
-end
+%end
